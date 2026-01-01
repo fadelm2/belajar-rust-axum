ca3ae9361798be7e95ed98e5b273ca6ea5a0e1b4
Bang Fadel <fadelmuahammad128@gmail.com>
2026-01-01 21:04:59 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use axum::{serve, Router};
+use axum::routing::{get, post};
+use axum_test::TestServer;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    serve(listener, app).await.unwrap();
+}
+
+#[tokio::test]
+async fn test_axum() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/").await;
+
+    response.assert_status_ok();
+    response.assert_text("Hello, World!");
+}
+
+#[tokio::test]
+async fn test_method_routing() {
+    async fn hello_world() -> String {
+        "Hello, World!".to_string()
+    }
+
+    let app = Router::new()
+        .route("/get", get(hello_world))
+        .route("/post", post(hello_world));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").await;
+    response.assert_status_ok();
+    response.assert_text("Hello, World!");
+
+    let response = server.post("/post").await;
+    response.assert_status_ok();
+    response.assert_text("Hello, World!");
+}
+
+
