5f7296be4c6c0192f1741c519a563f2b9b53a42d
Bang Fadel <fadelmuhammad128@gmail.com>
2026-01-03 12:45:46 +0700

    selesai axum

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::collections::HashMap;
-use axum::{serve, Form, Json, Router};
 use axum::body::{Body, Bytes};
-use axum::extract::{ Multipart ,Path, Query, Request};
 use axum::extract::rejection::JsonRejection;
-use axum::routing::{get, post};
-use axum_test::TestServer;
-use http::{HeaderMap, Method, StatusCode, Uri};
-use tokio::net::TcpListener;
-use serde::{Deserialize, Serialize};
-use log::{error, Log};
+use axum::extract::{Multipart, Path, Query, Request, State};
+use axum::middleware::{Next, from_fn, map_request};
 use axum::response::{IntoResponse, Response};
+use axum::routing::{get, post};
+use axum::{Form, Json, Router, serve, Extension};
+use axum_extra::extract::CookieJar;
+use axum_extra::extract::cookie::Cookie;
+use axum_test::TestServer;
 use axum_test::multipart::{MultipartForm, Part};
+use http::{HeaderMap, Method, StatusCode, Uri};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::Arc;
+use axum::error_handling::HandleError;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
@@ -52,8 +56,6 @@ async fn test_method_routing() {
     response.assert_text("Hello, World!");
 }
 
-
-
 #[tokio::test]
 async fn test_request() {
     async fn hello_world(request: Request) -> String {
@@ -68,9 +70,7 @@ async fn test_request() {
     let response = server.get("/get").await;
     response.assert_status_ok();
     response.assert_text("Hello, GET!");
-
 }
-
 
 #[tokio::test]
 async fn test_uri() {
@@ -84,7 +84,6 @@ async fn test_uri() {
     let response = server.get("/uri").await;
     response.assert_status_ok();
     response.assert_text("Hello, GET /uri!");
-
 }
 
 #[tokio::test]
@@ -99,10 +98,7 @@ async fn test_query() {
     let response = server.get("/query").add_query_param("name", "Fadel").await;
     response.assert_status_ok();
     response.assert_text("Hello, Fadel!");
-
 }
-
-
 
 #[tokio::test]
 async fn test_header() {
@@ -117,7 +113,6 @@ async fn test_header() {
     let response = server.get("/get").add_header("name", "Fadel").await;
     response.assert_status_ok();
     response.assert_text("Hello, Fadel!");
-
 }
 
 #[tokio::test]
@@ -132,9 +127,7 @@ async fn test_path_parameter() {
     let response = server.get("/products/1/categories/2").await;
     response.assert_status_ok();
     response.assert_text("Product 1, Category 2");
-
 }
-
 
 #[tokio::test]
 async fn test_body_string() {
@@ -148,33 +141,31 @@ async fn test_body_string() {
     let response = server.get("/post").text("This is Body").await;
     response.assert_status_ok();
     response.assert_text("Body This is Body");
-
 }
 
-#[derive(Debug,Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct LoginRequest {
     username: String,
     password: String,
 }
 
-
 #[tokio::test]
 async fn test_json() {
-    async fn hello_world(Json(request):Json<LoginRequest>) -> String {
+    async fn hello_world(Json(request): Json<LoginRequest>) -> String {
         format!("Hello {}", request.username)
     }
 
     let app = Router::new().route("/post", get(hello_world));
-    let request = LoginRequest { username: "Fadel".into(), password: "Fadel".into() };
-
+    let request = LoginRequest {
+        username: "Fadel".into(),
+        password: "Fadel".into(),
+    };
 
     let server = TestServer::new(app).unwrap();
     let response = server.get("/post").json(&request).await;
     response.assert_status_ok();
     response.assert_text("Hello Fadel");
-
 }
-
 
 #[tokio::test]
 async fn test_json_error() {
@@ -204,7 +195,6 @@ async fn test_json_error() {
     let response = server.post("/post").text("tidak valid").await;
     response.assert_status_ok();
     response.assert_text("Error MissingJsonContentType(MissingJsonContentType)");
-
 }
 
 #[tokio::test]
@@ -234,14 +224,15 @@ struct LoginResponse {
 #[tokio::test]
 async fn test_response_json() {
     async fn hello_world() -> (Response<()>, Json<LoginResponse>) {
-        ( Response::builder()
-            .status(StatusCode::OK)
-            .header("X-Owner", "Fadel")
-            .body(())
-            .unwrap(),
-        Json(LoginResponse{
-            token: "token".to_string(),
-        }),
+        (
+            Response::builder()
+                .status(StatusCode::OK)
+                .header("X-Owner", "Fadel")
+                .body(())
+                .unwrap(),
+            Json(LoginResponse {
+                token: "token".to_string(),
+            }),
         )
     }
 
@@ -253,19 +244,19 @@ async fn test_response_json() {
     response.assert_text("{\"token\":\"token\"}");
     response.assert_header("X-Owner", "Fadel");
 }
-
 
 #[tokio::test]
 async fn test_response_tupple() {
     async fn hello_world() -> (Response<()>, Json<LoginResponse>) {
-        ( Response::builder()
-              .status(StatusCode::OK)
-              .header("X-Owner", "Fadel")
-              .body(())
-              .unwrap(),
-          Json(LoginResponse{
-              token: "token".to_string(),
-          }),
+        (
+            Response::builder()
+                .status(StatusCode::OK)
+                .header("X-Owner", "Fadel")
+                .body(())
+                .unwrap(),
+            Json(LoginResponse {
+                token: "token".to_string(),
+            }),
         )
     }
 
@@ -277,7 +268,6 @@ async fn test_response_tupple() {
     response.assert_text("{\"token\":\"token\"}");
     response.assert_header("X-Owner", "Fadel");
 }
-
 
 #[tokio::test]
 async fn test_response_tupple3() {
@@ -285,11 +275,12 @@ async fn test_response_tupple3() {
         let mut header = HeaderMap::new();
         header.insert("X-Owner", "Fadel".parse().unwrap());
 
-        ( StatusCode::OK,
-          header,
-          Json(LoginResponse{
-              token: "token".to_string(),
-          }),
+        (
+            StatusCode::OK,
+            header,
+            Json(LoginResponse {
+                token: "token".to_string(),
+            }),
         )
     }
 
@@ -336,7 +327,6 @@ async fn test_multipart() {
 
         assert!(profile.len() > 0);
         format!("Hello {}", username)
-
     }
 
     let app = Router::new().route("/post", post(hello_world));
@@ -352,3 +342,313 @@ async fn test_multipart() {
     response.assert_text("Hello Eko");
 }
 
+#[tokio::test]
+async fn test_cookie_response() {
+    async fn hello_world(query: Query<HashMap<String, String>>) -> (CookieJar, String) {
+        let name = query.get("name").unwrap();
+        (
+            CookieJar::new().add(Cookie::new("name", name.clone())),
+            format!("Hello {}", name.clone()),
+        )
+    }
+
+    let app = Router::new().route("/get", get(hello_world));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").add_query_param("name", "Fadel").await;
+    response.assert_status_ok();
+    response.assert_text("Hello Fadel");
+    response.assert_header("Set-Cookie", "name=Fadel");
+}
+
+#[tokio::test]
+async fn test_cookie_request() {
+    async fn hello_world(cookie: CookieJar) -> String {
+        let name = cookie.get("name").unwrap().value();
+
+        format!("Hello {}", name)
+    }
+
+    let app = Router::new().route("/get", get(hello_world));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").add_header("Cookie", "name=Fadel").await;
+    response.assert_status_ok();
+    response.assert_text("Hello Fadel");
+}
+
+async fn log_middleware(request: Request, next: Next) -> Response {
+    println!(" Receive request {} {}", request.method(), request.uri());
+    let response = next.run(request).await;
+    println!("Send response {}", response.status());
+    response
+}
+
+async fn request_id_middleware<T>(mut request: Request<T>) -> Request<T> {
+    let request_id = "123456";
+    request
+        .headers_mut()
+        .insert("X-Request-Id", request_id.parse().unwrap());
+    request
+}
+
+#[tokio::test]
+async fn test_middleware() {
+    async fn hello_world(method: Method, header_map: HeaderMap) -> String {
+        println!("Execute handler");
+        let request_id = header_map.get("X-Request-Id").unwrap().to_str().unwrap();
+        format!("Hello {} {}", method, request_id)
+    }
+
+    let app = Router::new()
+        .route("/get", get(hello_world))
+        .layer(map_request(request_id_middleware))
+        .layer(from_fn(log_middleware));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").add_header("Cookie", "name=Eko").await;
+    response.assert_status_ok();
+    response.assert_text("Hello GET 123456");
+}
+
+struct AppError {
+    code: i32,
+    message: String,
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::from_u16(self.code as u16).unwrap(),
+            self.message,
+        )
+            .into_response()
+    }
+}
+
+#[tokio::test]
+async fn test_error_handling() {
+    async fn hello_world(method: Method) -> Result<String, AppError> {
+        if method == Method::POST {
+            Ok("OK".to_string())
+        } else {
+            Err (AppError{
+                code :400,
+                message: "Bad Request".to_string(),
+            })
+        }
+
+    }
+    let app = Router::new()
+        .route("/get", get(hello_world))
+    .route("/post", post(hello_world));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").await;
+    response.assert_status(StatusCode::BAD_REQUEST);
+    response.assert_text("Bad Request");
+
+    let response = server.post("/post").await;
+    response.assert_status(StatusCode::OK);
+    response.assert_text("OK");
+
+}
+
+#[tokio::test]
+async fn test_unexpected_handling() {
+    async fn route(request: Request) -> Result<Response, anyhow::Error> {
+        if request.method() == Method::POST {
+            Ok(Response::builder()
+                .status(StatusCode::OK)
+                .body(Body::from("OK"))?)
+        } else {
+            Err (anyhow::Error::msg("Bad Request"))
+        }
+
+    }
+
+    async fn handle_error(err: anyhow::Error) -> (StatusCode, String) {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Internal Server Error: {}", err),
+            )
+    }
+
+    let route_service = tower::service_fn(route);
+
+    let app = Router::new()
+        .route_service("/get", HandleError::new(route_service, handle_error));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").await;
+
+    response.assert_status(StatusCode::INTERNAL_SERVER_ERROR);
+    response.assert_text("Internal Server Error: Bad Request");
+
+}
+
+struct DatabaseConfig {
+    total : i32,
+}
+
+
+#[tokio::test]
+async fn test_state_extractor() {
+    let database_state = Arc::new(DatabaseConfig{total : 100});
+
+    async fn hello_world(State(database) : State<Arc<DatabaseConfig>>) -> String {
+        format!("Total {}", database.total)
+    }
+
+    let app = Router::new()
+        .route("/get", get(hello_world))
+        .with_state(database_state);
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").await;
+    response.assert_status_ok();
+    response.assert_text("Total 100");
+}
+
+#[tokio::test]
+async fn test_state_extension() {
+    let database_state = Arc::new(DatabaseConfig{total : 100});
+
+    async fn hello_world(Extension(database) : Extension<Arc<DatabaseConfig>>) -> String {
+        format!("Total {}", database.total)
+    }
+
+    let app = Router::new()
+        .route("/get", get(hello_world))
+        .layer(Extension(database_state));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").await;
+    response.assert_status_ok();
+    response.assert_text("Total 100");
+}
+
+
+
+#[tokio::test]
+async fn test_state_closure_capture() {
+    let database_state = Arc::new(DatabaseConfig { total: 100 });
+
+    async fn hello_world(database: Arc<DatabaseConfig>) -> String {
+        format!("Total {}", database.total)
+    }
+    let app = Router::new()
+        .route("/get", get({
+            let database_state = Arc::clone(&database_state);
+            move || hello_world(database_state)
+        }),
+        )
+        .layer(Extension(database_state));
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/get").await;
+    response.assert_status_ok();
+    response.assert_text("Total 100");
+}
+
+
+#[tokio::test]
+async fn test_multiple_routes() {
+    async fn hello_world(method: Method) -> String {
+        format!("Hello {}", method)
+    }
+
+    let first_route = Router::new().route("/first", get(hello_world));
+    let second_route = Router::new().route("/second", get(hello_world));
+
+    let app= Router::new().merge(first_route).merge(second_route);
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/first").await;
+    response.assert_status_ok();
+    response.assert_text("Hello GET");
+
+    let response = server.get("/second").await;
+    response.assert_status_ok();
+    response.assert_text("Hello GET");
+
+}
+
+
+#[tokio::test]
+async fn test_multiple_route_nest() {
+    async fn hello_world(method: Method) -> String {
+        format!("Hello {}", method)
+    }
+
+    let first_route = Router::new().route("/first", get(hello_world));
+    let second_route = Router::new().route("/second", get(hello_world));
+
+    let app= Router::new()
+        .nest("/api/users", first_route)
+        .nest("/api/products", second_route);
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/api/users/first").await;
+    response.assert_status_ok();
+    response.assert_text("Hello GET");
+
+    let response = server.get("/api/products/second").await;
+    response.assert_status_ok();
+    response.assert_text("Hello GET");
+
+}
+
+
+#[tokio::test]
+async fn test_fallback() {
+    async fn hello_world(method: Method) -> String {
+        format!("Hello {}", method)
+    }
+
+    async fn fallback(request: Request) -> (StatusCode, String) {
+        (
+            StatusCode::NOT_FOUND,
+            format!("Page {} is not found", request.uri().path()),
+            )
+    }
+
+
+    async fn not_allowed(request: Request) -> (StatusCode, String) {
+        (
+            StatusCode::METHOD_NOT_ALLOWED,
+            format!("Page {} is not found", request.uri().path()),
+        )
+    }
+
+
+
+
+
+    let first_route = Router::new().route("/first", get(hello_world));
+    let second_route = Router::new().route("/second", get(hello_world));
+
+    let app= Router::new()
+        .merge(first_route)
+        .merge(second_route)
+        .fallback(fallback)
+        .method_not_allowed_fallback(not_allowed);
+
+    let server = TestServer::new(app).unwrap();
+    let response = server.get("/first").await;
+    response.assert_status_ok();
+    response.assert_text("Hello GET");
+
+    let response = server.get("/second").await;
+    response.assert_status_ok();
+    response.assert_text("Hello GET");
+
+    let response = server.get("/wrong").await;
+    response.assert_status_not_found();
+    response.assert_text("Page /wrong is not found");
+
+    let response = server.post("/first").await;
+    response.assert_status(StatusCode::METHOD_NOT_ALLOWED);
+    response.assert_text("Page /first is not found");
+
+}
